7d8bf50568b411daacf101c8bb3198b299fd2408
rijalsinaga <rsrizalsinaga@gmail.com>
2019-09-15 21:31:50 +0700

    modified rekening1.index.blade.php

--- resources/views/rekening1/index.blade.php
+++ resources/views/rekening1/index.blade.php
@@ -2,6 +2,64 @@
 
 @section('content')
     
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+				<div class="container-fluid">
+					<h3 class="page-title">Tables</h3>
+					<div class="row">
+						<div class="col-md-6">
+							<!-- TABLE HOVER -->
+							<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Hover Row</h3>
+								</div>
+								<div class="panel-body">
+									<table class="table table-hover">
+										<thead>
+											<tr>
+												<th>#</th>
+												<th>First Name</th>
+												<th>Last Name</th>
+												<th>Username</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>Steve</td>
+												<td>Jobs</td>
+												<td>@steve</td>
+											</tr>
+											<tr>
+												<td>2</td>
+												<td>Simon</td>
+												<td>Philips</td>
+												<td>@simon</td>
+											</tr>
+											<tr>
+												<td>3</td>
+												<td>Jane</td>
+												<td>Doe</td>
+												<td>@jane</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+							<!-- END TABLE HOVER -->
+						</div>
+					</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- END MAIN CONTENT -->
+		
+@endsection
+
+
+{{-- @section('content')
+    
 @if (session('success'))
 <div class="alert alert-info" role="alert">
     {{ session ('success') }}
@@ -74,4 +132,4 @@
     </div>
   </div>
 </div>
-@endsection
+@endsection --}}
