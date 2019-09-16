657b93919bd9d74e1405029666f5c6232ba53cde
rijalsinaga <rsrizalsinaga@gmail.com>
2019-09-17 00:10:52 +0700

    "add css bootstrap & _fungsiJavaScript/_fungsiJs"

---
+++ resources/views/_fungsiJavaScript/_fungsiJs.Blade.php
@@ -0,0 +1,17 @@
+<!-- DESAIN CSS -->
+<link rel="stylesheet" href="{{asset('desain/bootstrap-theme.css')}}">
+<link rel="stylesheet" href="{{asset('desain/css.css')}}">
+<link rel="stylesheet" href="{{asset('desain/elegant-icons-style.css')}}">
+<link rel="stylesheet" href="{{asset('font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('line-icons.css')}}">
+<link rel="stylesheet" href="{{asset('line-icons.css')}}">
+<link rel="stylesheet" href="{{asset('style.css')}}">
+<link rel="stylesheet" href="{{asset('style-responsive.css')}}"> --}}
+<!-- END DESAIN CSS -->
+
+function() {
+  return e.apply(n || this, r.concat(g.call(arguments)))
+}
+function() {
+  return e.apply(n || this, r.concat(g.call(arguments)))
+}
